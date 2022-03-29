6ad345b9d72acbd912addb0ead1153b23444c787
mihaivladimir25 <mihaicojocaru50@gmail.com>
2022-03-29 09:32:43 +0300

    prtima parte

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::process;
+use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, write}};
+
+fn main(){
+
+    let mut cit = String::new();
+    print!("introdu un nume");
+    let b1 = std::io::stdin().read_line(&mut cit).unwrap();
+    let b = Box::new(String::from("ssss ".to_owned() + &cit));
+
+    // cit = "ssss ".to_owned() + &cit;
+
+    println!("{}", b);
+
+    
+
+}
