54b10c8ac2d2a1f3fc375937352bb4e3615ad221
mihaivladimir25 <mihaicojocaru50@gmail.com>
2022-03-30 10:38:43 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, write}};
 
 fn main(){
 
-    let mut cit = String::new();
-    print!("introdu un nume");
-    let b1 = std::io::stdin().read_line(&mut cit).unwrap();
-    let b = Box::new(String::from("ssss ".to_owned() + &cit));
+    // let mut cit = String::new();
+    // print!("introdu un nume");
+    // let b1 = std::io::stdin().read_line(&mut cit).unwrap();
+    // let b = Box::new(String::from("ssss ".to_owned() + &cit));
 
-    // cit = "ssss ".to_owned() + &cit;
+    // // cit = "ssss ".to_owned() + &cit;
 
-    println!("{}", b);
+    // println!("{}", b);
 
     
 
